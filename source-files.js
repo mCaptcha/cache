47e7653c674f5672dc56dabaa626adc9065a5756
realaravinth <realaravinth@batsense.net>
2021-06-06 04:54:07 +0000

    Deploying to gh-pages from  @ d0a67a712acc3cfa1d8fd1d278b08381a9e0e941 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["cache"] = {"name":"","files":["errors.rs","lib.rs","pocket.rs","utils.rs"]};
+sourcesIndex["cache"] = {"name":"","files":["errors.rs","lib.rs","mcaptcha.rs","pocket.rs","utils.rs"]};
 createSourceSidebar();
