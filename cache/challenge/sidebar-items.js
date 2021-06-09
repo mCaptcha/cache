a284d0d56fa34133102fdf66bcbc5d83853e9d8e
realaravinth <realaravinth@batsense.net>
2021-06-09 14:53:29 +0000

    Deploying to gh-pages from  @ ca8709fd93d9aadfc24e1da5b01c0bd72823f96e 🚀

--- cache/challenge/sidebar-items.js
+++ cache/challenge/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MCAPTCHA_CHALLENGE_VERSION",""]],"mod":[["type_methods",""]],"static":[["MCAPTCHA_CHALLENGE_TYPE",""]],"struct":[["AddChallenge",""],["Challenge",""]]});
+initSidebarItems({"constant":[["MCAPTCHA_CHALLENGE_VERSION",""]],"mod":[["type_methods",""]],"static":[["MCAPTCHA_CHALLENGE_TYPE",""]],"struct":[["Challenge",""]]});
