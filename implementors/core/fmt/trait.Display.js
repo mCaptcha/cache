d68586ff3e8c41ee73a44858cb1a60339d98c4bb
realaravinth <realaravinth@batsense.net>
2021-12-06 06:51:41 +0000

    Deploying to gh-pages from  @ efb67a4a8532cfa1aab8fa7f7cc671664e654291 🚀

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["cache"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cache/errors/enum.CacheError.html\" title=\"enum cache::errors::CacheError\">CacheError</a>","synthetic":false,"types":["cache::errors::CacheError"]}];
+implementors["cache"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cache/errors/enum.CacheError.html\" title=\"enum cache::errors::CacheError\">CacheError</a>","synthetic":false,"types":["cache::errors::CacheError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
